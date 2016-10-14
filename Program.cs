e47038de0f82c15906b36a30e44d94029cb27ea3
Harm Hofstee <harm@tallcomponents.com>
2016-10-14 15:01:03 +0200

    Now SimpleXhtmlShape

--- Program.cs
+++ Program.cs
@@ -19,88 +19,77 @@ namespace SimpleXhtmlShapeFontSize
             Document document = new Document();
             Section section = document.Sections.Add();
 
-            XhtmlParagraph xhtmlParagraph = new XhtmlParagraph();
-
-            ConversionSettings conversionSettings = new ConversionSettings();
-
-            xhtmlParagraph.Settings = conversionSettings;
-            xhtmlParagraph.Text = Html();
-
-            //section.Paragraphs.Add(xhtmlParagraph);
-
-            Drawing drawing = new Drawing(200, 200);
-            section.Paragraphs.Add(drawing);
-            PieShape pie = new PieShape();
-            pie.Pen = new Pen(System.Drawing.Color.Red, 2);
-            pie.Brush = new SolidBrush(System.Drawing.Color.Blue);
-            drawing.Shapes.Add(pie);
-
-            //drawing.Shapes.Add(new SimpleXhtmlShape()
-            //        {
-            //            DefaultFontSize = 18,
-            //            Text = Html(),
-            //        }
-            //);
+            FontsMapping(section, 8);
+            FontsMapping(section, 12);
+            FontsMapping(section, 18);
 
             using (var file = new FileStream(@"..\..\result.pdf", FileMode.Create, FileAccess.Write))
             {
                 document.Write(file);
             }
         }
-
-        static private String Html()
+        static private Drawing FontsMapping(Section section, Int32 fontsize)
         {
-            return @"
+            Drawing drawing = new Drawing(
+                    section.PageSize.Width - section.Margin.Left - section.Margin.Right,
+                    section.PageSize.Height - section.Margin.Top - section.Margin.Bottom);
+            section.Paragraphs.Add(drawing);
+
+            SimpleXhtmlShape shape = new SimpleXhtmlShape();
+            shape.Dock = DockStyle.Top;
+            shape.DefaultFontSize = fontsize;
+            shape.Text = Html(fontsize);
+            shape.Width = drawing.Width;
+            drawing.Shapes.Add(shape);
+
+            return drawing;
+        }
+
+
+        static private String Html(Int32 fontsize)
+        {
+            return (@"
                         <body>
-                            <h1>Fontsizes in html</h1>
+                            <p style='font-size: xx-large;'>Fontsizes in html: @FS@</p>
+                            <p></p>
                     " +
-                            Table() +
-                   @"       <p>
-                                'larger' and 'smaller' ar relative to the surrounding or default fontsize.
-                            </p>
+                            Table(fontsize) +
+                   @"       <p>'larger' and 'smaller' ar relative to the surrounding or default fontsize.</p>
+                            <p></p>
                         </body>
-                    ";
+                    ").Replace("@FS@", fontsize.ToString() + "pt");
         }
 
-        static private String Table()
+        static private String Table(Int32 fontsize)
         {
-            return @" 
-                    <table style='border: 1px solid black;'>
-                        <tr>
-                            <th style='border: 1px solid black;'>Named</th>
-                            <th style='border: 1px solid black;'>Coded</th>
-                        </tr>
-                    " +
-                    Row("xx-small", "4pt") +
-                    Row("x-small",  "6pt") +
-                    Row("smaller",  "80%") +
-                    Row("small",    "10pt") +
-                    Row("medium",   "12pt") +
-                    Row("large",    "14pt") +
-                    Row("larger",   "120%") +
-                    Row("x-large",  "18pt") +
-                    Row("xx-large", "20pt") +
-                    @"
-                    </table>
-                    ";
+            return
+                    Row(1, "xx-small", 8, "8pt") +
+                    Row(1, "x-small", 10, "10pt") +
+                    Row(1, "small", 12, "12pt") +
+                    Row(1, "medium", 16, "16pt") +
+                    Row(1, "large", 20, "20pt") +
+                    Row(1, "x-large", 24, "24pt") +
+                    Row(1, "xx-large", 36, "36pt") +
+                    Row(fontsize, "smaller", 0.8, "0.8 * @FS@") +
+                    Row(fontsize, "larger", 1.2, "1.2 * @FS@") +
+                    "";
         }
 
-        static private String Row(String named, String coded)
+        static private String Row(Int32 fontsize, String named, double factor, String coded)
         {
-            return @"
-                     <tr>
-                    " + Column(named) +
-                        Column(coded) +
-                  @"
-                    </tr>
-                   ";
+            string fs = (fontsize * factor).ToString() + "pt";
+
+            return ("<p>" + Column(named) + "<span style='font-size: @fs@;'> -- </span>" + Column(fs, coded) + "</p>").Replace("@fs@", named);
         }
 
         static private String Column(String name)
         {
-            return @"
-                        <td style='border: 1px solid black; font-size: @Name@;'> @Name@</td>
-                    ".Replace("@Name@", name);
+            return @"<span style='font-size: @Name@;'> @Name@</span>".Replace("@Name@", name);
+        }
+
+        static private String Column(string fs, String name)
+        {
+            return @"<span style='font-size: @FontSize@;'> @Name@</span>".Replace("@Name@", name).Replace("@FontSize@", fs);
         }
     }
 }
